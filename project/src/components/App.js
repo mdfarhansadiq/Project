0dff47851cacbb6f2438d20b610337d39969eb41
samiul19 <mdfarhansadiq01@gmail.com>
2022-11-21 17:00:59 +0600

    Project

---
+++ project/src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Title from './title/title';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>React Lecture</h1>
+        <Title name = "Farhan"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
