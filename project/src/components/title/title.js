3e84c549d883af82f91dd0597341fd9755260c5d
Md. Farhan Sadiq <mdfarhansadiq01@gmail.com>
2022-12-07 12:26:48 +0600

    code

--- project/src/components/title/title.js
+++ project/src/components/title/title.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import './title.css';
 
 class Title extends Component {
 
@@ -11,16 +12,32 @@ class Title extends Component {
       }
     }
 
+    editHandler() {
+        this.setState({
+            isInput: this.state.isInput = true
+        })
+    }
+
     render (){
         let output = null;
         const {title, isInput} = this.state;
         if(isInput){
-
+            output = (
+                <div className="Title">
+                    <input
+                        className="form-control"
+                        onChange={event => this.inputChange(event)}
+                        onKeyPress={event => this.keyPressHandler(event)}
+                        onBlur={event => this.blurHandler(event)}
+                        type="text"
+                        value={this.state.title}
+                    />
+                </div>)
         }
         else{
             output = (
                 <div className="d-flex Title">
-                    <h1 className="display-4"> {this.state.title} </h1>
+                    <h1 className="display-4"> {title} </h1>
                     <span onClick={() => this.editHandler()} className="ml-auto edit-icon">
                         <i className="fas fa-pencil-alt"></i>
                     </span>
